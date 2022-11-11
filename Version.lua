b78950637287f85daf5269c34168bbbf12a7f0b7
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 16:13:41 +0200

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,9 +1,9 @@
 local VerifyVersion = 1
 
 if versionCURRENT ~= VerifyVersion then
-AlertUI = AlertI.Label({Text = "Your Using A Outdated Version Please Update"})
+AlertList = AlertTab({Text = "Your Using A Outdated Version Please Update"})
   OUTDATEDVERSION = true
 else
-  AlertUI = AlertI.Label({Text = "Your Running The Latest Version"})
+  AlertList = AlertTab({Text = "Your Running The Latest Version"})
   OUTDATEDVERSION = false
 end
