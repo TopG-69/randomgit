55f00a68a0d67343cf0ae3fef5f47f6670426069
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 18:05:30 +0200

    Create Ultimate Driving Westover.lua

---
+++ Ultimate Driving Westover.lua
@@ -0,0 +1,26 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Module.lua"))()
+local UI = Library.Load({
+	Title = "Ultimate Driving Westover",
+	Style = 1,
+	SizeX = 600,
+	SizeY = 400,
+	Theme = "Light",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(25,25,25 ),
+		Minimise = Color3.fromRGB(0, 0, 0 )
+	}
+})
+
+WelcomeUI = UI.New({Title = "Welcome"})
+
+if PremiumKey == "EJOHFDNSIBTREKPQWNDIASDHUBEJNOWBNTEORBNJSACNJOSVNIDASNOVUESNAVUBORTB" then
+WelcomeI = WelcomeUI.Label({Text = "Premium is True"})
+	local Premium = true
+else
+WelcomeI = WelcomeUI.Label({Text = "Premium is false"})
+	local Premium = false
+end
+
+WelcomeI = WelcomeUI.CircleButton({Text = "Click To Copy Invite", function()
+    setclipboard("https://discord.gg/DYkHGnYeDp")
+end, })
