fef38364c398884b61d97c942d85f2721d14757c
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 19:16:34 +0200

    Update Ultimate Driving.lua

--- Ultimate Driving.lua
+++ Ultimate Driving.lua
@@ -10,6 +10,24 @@ local UI = Library.Load({
 		Minimise = Color3.fromRGB(0, 0, 0 )
 	}
 })
+--[--Functions
+
+function SetWalkSpeed(value)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end
+
+function SetJumpPower(value)
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+end
+
+function RestoreWSJP()
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+end
+
+--]--End Of Functions
+
+
 
 --[--Sound
 local s = Instance.new ("Sound", workspace)
@@ -17,6 +35,8 @@ s.SoundId = 'rbxassetid://530637073'
 s.Volume = 1
 s:Play()
 --]--End Of Sound
+
+
 
 --[--Welcome
 WelcomeUI = UI.New({Title = "Welcome"})
@@ -38,10 +58,28 @@ end, })
 
 
 
+--[--Local
+LocalUI = UI.New({Title = "Local"})
+
+--Start Of Code
+LocalI = LocalUI.TextField({Text = "WalkSpeed", Callback = function(v)
+	SetWalkSpeed(v)
+end, })
+
+LocalI = LocalUI.TextField({Text = "JumpPower", Callback = function(v)
+	SetJumpPower(v)
+end, })
+
+LocalI = LocalUI.Button({Text = "Restore WS/JP", Callback = function()
+        RestoreWSJP()
+end, })
+--]--End Of Local
+
+
+
 --[--Premium
 --*
 
 --Start Of Code
 PremiumI = PremiumUI.Label({Text = "No Options Yet"})
 --]--End Of Premium
-
