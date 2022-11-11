f81f0eb3013d3370b40c3d15b07ea8c0cef209aa
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 15:26:04 +0200

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,5 +1,7 @@
-local VerifyVersion = "1"
+local VerifyVersion = 1
 
 if versionCURRENT ~= VerifyVersion then
 AlertUI = Alert.Label({Text = "Your Using A Outdated Version Please Update"})
+else
+  AlertUI = Alert.Label({Text = "Your Running The Latest Version"})
 end
