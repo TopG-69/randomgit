7525ff9077a9184f3b0c6d380d9386983c07d02f
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 15:52:54 +0200

    Create LoadModules.lua

---
+++ LoadModules.lua
@@ -0,0 +1,13 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Module.lua"))()
+local UI = Library.Load({
+	Title = "Fishing Simulator - Script By ?...",
+	Style = 1,
+	SizeX = 600,
+	SizeY = 400,
+	Theme = "Light",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(25,25,25 ),
+		Minimise = Color3.fromRGB(0, 0, 0 )
+	}
+})
+wait(1)
