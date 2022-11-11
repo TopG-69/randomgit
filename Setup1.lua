4f478f1550dca79fd20b829b9a91e41543ad0df0
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 16:47:52 +0200

    Update Setup1.lua

--- Setup1.lua
+++ Setup1.lua
@@ -1,2 +1,35 @@
-AlertTab = UI.New({Title = "Alerts"})
+if game.PlaceId == 2866967438 and OUTDATEDVERSION == false then
+AlertList = AlertTab.Label({Text = "Fishing Simulator Script Is Loading"})
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Module2.lua", true))()
+elseif game.PlaceId == 0 and OUTDATEDVERSION == false then
+elseif game.PlaceId == 0 and OUTDATEDVERSION == false then
+elseif OUTDATEDVERSION == true then
+AlertList = AlertTab.Label({Text = "Error1! - ODV/VT"})
 wait(0.1)
+AlertList = AlertTab.Label({Text = "Possible Causes"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Your Version Is Invalid Update At Discord Server"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "You Copied The Script Falsely"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Still Need Help? Join The Discord - https://discord.gg/DYkHGnYeDp"})
+wait(0.1)
+AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
+    setclipboard("https://discord.gg/DYkHGnYeDp")
+end, })
+elseif game.PlaceId ~= 2866967438 or game.PlaceId == 0 or game.PlaceId == 0 and OUTDATEDVERSION == false then
+AlertList = AlertTab.Label({Text = "Error2! - NG/WD"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Possible Causes"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Your Game Isint Supported"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "The Game Wasent Detected"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Still Need Help? Join The Discord - https://discord.gg/DYkHGnYeDp"})
+wait(0.1)
+AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
+    setclipboard("https://discord.gg/DYkHGnYeDp")
+end, })
+end
