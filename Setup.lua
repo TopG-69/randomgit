c5ef9d8fd826bddd036fd56a1ec657ad5c5b22d7
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-11 21:18:32 +0200

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -17,23 +17,41 @@ local UI = Library.Load({
 AlertTab = UI.New({Title = "Alerts"})
 wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Version.lua", true))()
+BrokenThemes = Theme ~= "Aqua" or Theme ~= "Light" or Theme ~= "Dark" or Theme ~= "Jester" or Theme ~= "Mocha" or Theme == nil
+FixedThemes = Theme == "Aqua" or Theme == "Light" or Theme == "Dark" or Theme == "Jester" or Theme == "Mocha"
 --]--End Of Setup
 
 --[--Script
-if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "123" then
+if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
 AlertList = AlertTab.Label({Text = "Fishing Simulator Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Fishing%20Simulator.lua", true))()
-elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "123" then
+elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
 AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/ThemeParkTycoon2.lua", true))()
-elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" then
+elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
 AlertList = AlertTab.Label({Text = "Ultimate Driving Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Ultimate%20Driving.lua", true))()
-elseif Discord_Key ~= "123" then
-AlertList = AlertTab.Label({Text = "Error1! - NK"})
+elseif BrokenThemes then
+AlertList = AlertTab.Label({Text = "Error1! - NT"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Possible Causes"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Your Inputted Color Is Wrong"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Your Missing Theme ="})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "To Disable This Error Put Theme = 1"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Still Need Help? Join The Discord - https://discord.gg/DYkHGnYeDp"})
+wait(0.1)
+AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
+    setclipboard("https://discord.gg/DYkHGnYeDp")
+end, })
+elseif Discord_Key ~= "123" and FixedThemes then
+AlertList = AlertTab.Label({Text = "Error2! - NK"})
 wait(0.1)
 AlertList = AlertTab.Label({Text = "Possible Causes"})
 wait(0.1)
@@ -46,8 +64,8 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard("https://discord.gg/DYkHGnYeDp")
 end, })
-elseif OUTDATEDVERSION == true and Discord_Key == "123" then
-AlertList = AlertTab.Label({Text = "Error2! - ODV/VT"})
+elseif OUTDATEDVERSION == true and Discord_Key == "123" and FixedThemes then
+AlertList = AlertTab.Label({Text = "Error3! - ODV/VT"})
 wait(0.1)
 AlertList = AlertTab.Label({Text = "Possible Causes"})
 wait(0.1)
@@ -60,8 +78,8 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard("https://discord.gg/DYkHGnYeDp")
 end, })
-elseif game.PlaceId ~= 2866967438 or game.PlaceId == 69184822 or game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" then
-AlertList = AlertTab.Label({Text = "Error3! - NG/WD"})
+elseif game.PlaceId ~= 2866967438 or game.PlaceId == 69184822 or game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
+AlertList = AlertTab.Label({Text = "Error4! - NG/WD"})
 wait(0.1)
 AlertList = AlertTab.Label({Text = "Possible Causes"})
 wait(0.1)
