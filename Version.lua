2e2c04527e9de6d353c687ea525a2fe95be2e7fe
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-12 15:17:10 +0200

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -3,9 +3,12 @@ local VerifyVersion = 1
 if versionCURRENT ~= VerifyVersion then
   AlertList = AlertTab.Label({Text = "Your Using A Outdated Version Please Update"})
   OUTDATEDVERSION = true
-else
+elseif versionCURRENT == VerifyVersion then
   AlertList = AlertTab.Label({Text = "Your Running The Latest Version"})
   OUTDATEDVERSION = false
+else 
+  AlertList = AlertTab.Label({Text = "Unknown Issue With Version Verify"})
+  OUTDATEDVERSION = true
 end
 --]--End Of Verify Main Loader
 
@@ -17,20 +20,37 @@ wait(0.1)
 
 
 if FishingSimulatorCURRENT ~= FishingSimulatorVersion then
+AlertList = AlertTab.Label({Text = "Fishing Simulator Is Outdated"})
 FSISPATCHED = true
 elseif FishingSimulatorCURRENT == FishingSimulatorVersion then
+AlertList = AlertTab.Label({Text = "Fishing Simulator Is Running Latest Version"})
 FSISPATCHED = false
+else
+AlertList = AlertTab.Label({Text = "Unknown Issue Happined While Verifying Fishing Simulator Version"})
+FSISPATCHED = true
 end
 
 if ThemeParkTycoon2CURRENT ~= ThemeParkTycoon2Version then
+AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Is Outdated"})
 TPT2ISPATCHED = true
 elseif ThemeParkTycoon2CURRENT == ThemeParkTycoon2Version then
+AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Is Running Latest Version"})
 TPT2ISPATCHED = false
+else
+AlertList = AlertTab.Label({Text = "Unknown Issue Happined While Verifying Theme Park Tycoon 2 Version"})
+TPT2ISPATCHED = true
+else
+
 end
 
 if UltimateDrivingCURRENT ~= UltimateDrivingVersion then
+AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Is Outdated"})
 UDISPATCHED = true
 elseif UltimateDrivingCURRENT == UltimateDrivingVersion then
+AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Is Running Latest Version"})
 UDISPATCHED = false
+else
+AlertList = AlertTab.Label({Text = "Unknown Issue Happined While Verifying Ultimate Driving Version"})
+UDISPATCHED = true
 end
 --]--End Of Is Patched
