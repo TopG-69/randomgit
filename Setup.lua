bb24723289fb41fb0df026706b995a90ec0b82b0
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-13 01:02:16 +0200

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -45,14 +45,17 @@ if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "1
 AlertList = AlertTab.Label({Text = "Fishing Simulator Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Fishing%20Simulator.lua", true))()
+	
 elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and TPT2ISPATCHED == false and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/ThemeParkTycoon2.lua", true))()
+	
 elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and UDISPATCHED == false and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Ultimate Driving Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Ultimate%20Driving.lua", true))()
+	
 elseif BrokenThemes and OUTDATEDVERSION == false and Discord_Key == "123" and IsPatched and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Error1! - NT"})
 wait(0.1)
@@ -69,6 +72,7 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard('https://discord.gg/DYkHGnYeDp')
 end, })
+	
 elseif Discord_Key ~= "123" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Error2! - NK"})
 wait(0.1)
@@ -83,6 +87,7 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard('https://discord.gg/DYkHGnYeDp')
 end, })
+	
 elseif OUTDATEDVERSION == true and Discord_Key == "123" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Error3! - ODV/VT"})
 wait(0.1)
@@ -97,6 +102,7 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard('https://discord.gg/DYkHGnYeDp')
 end, })
+	
 elseif game.PlaceId ~= 2866967438 or game.PlaceId ~= 69184822 or game.PlaceId ~= 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
 AlertList = AlertTab.Label({Text = "Error4! - NG/WD"})
 wait(0.1)
@@ -113,6 +119,7 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard('https://discord.gg/DYkHGnYeDp')
 end, })
+	
 AlertList = AlertTab.Label({Text = "Loading Universal Version In 10 Secs"})
 wait(10)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/UniversalScript.lua", true))()
@@ -130,6 +137,7 @@ wait(0.1)
 AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
     setclipboard('https://discord.gg/DYkHGnYeDp')
 end, })
+	
 elseif ScreenSizeX == nil or ScreenSizeX == nan or ScreenSizeY == nil or ScreenSizeY == nan and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and UDISPATCHED == false then
 AlertList = AlertTab.Label({Text = "Error5! - BSX/BSY"})
 wait(0.1)
