3d7b31bd3a123d466a636876810e62cecbe6a03c
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-12 15:02:26 +0200

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -11,6 +11,9 @@ local UI = Library.Load({
 	}
 })
 --[--Run Checks
+FishingSimulatorCURRENT = 1
+ThemeParkTycoon2CURRENT = 1
+UltimateDrivingCURRENT = 1
 --]--Finished
 
 --[--Setup
@@ -19,18 +22,19 @@ wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Version.lua", true))()
 FixedThemes = Theme == "Aqua" or Theme == "Light" or Theme == "Dark" or Theme == "Jester" or Theme == "Mocha"
 BrokenThemes = Theme ~= "Aqua" or Theme ~= "Light" or Theme ~= "Dark" or Theme ~= "Jester" or Theme ~= "Mocha" or Theme == nil
+wait(0.1)
 --]--End Of Setup
 
 --[--Script
-if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
+if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and FSISPATCHED == false then
 AlertList = AlertTab.Label({Text = "Fishing Simulator Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Fishing%20Simulator.lua", true))()
-elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
+elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and TPT2ISPATCHED == false then
 AlertList = AlertTab.Label({Text = "Theme Park Tycoon 2 Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/ThemeParkTycoon2.lua", true))()
-elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes then
+elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and UDISPATCHED == false then
 AlertList = AlertTab.Label({Text = "Ultimate Driving Script Is Loading"})
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Ultimate%20Driving.lua", true))()
@@ -88,6 +92,20 @@ wait(0.1)
 AlertList = AlertTab.Label({Text = "The Game Wasent Detected"})
 wait(0.1)
 AlertList = AlertTab.Label({Text = "You Have Been Teleported To Another Game"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Still Need Help? Join The Discord - https://discord.gg/DYkHGnYeDp"})
+wait(0.1)
+AlertListCopy = AlertTab.Button({Text = "Click To Copy Invite", function()
+    setclipboard("https://discord.gg/DYkHGnYeDp")
+end, })
+elseif OUTDATEDVERSION == false and Discord_Key == "123" and FixedThemes and TPT2ISPATCHED == true or UDISPATCHED == true or FSISPATCHED == true then
+AlertList = AlertTab.Label({Text = "Error5! - SIP"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Possible Causes"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Script Is Patched"})
+wait(0.1)
+AlertList = AlertTab.Label({Text = "Your Using A Modded Version Of The Script"})
 wait(0.1)
 AlertList = AlertTab.Label({Text = "Still Need Help? Join The Discord - https://discord.gg/DYkHGnYeDp"})
 wait(0.1)
