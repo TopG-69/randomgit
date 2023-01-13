1034aca3dc280e70a023196f59699acc73187f80
DAa <103658363+Sittapea@users.noreply.github.com>
2023-01-13 21:41:24 +0200

    Update Fishing Simulator.lua

--- Fishing Simulator.lua
+++ Fishing Simulator.lua
@@ -1,14 +1,51 @@
+function CreateFloatingTextBox(text, time, colorr, colorg, colorb)
+PopupMessage = Instance.new("ScreenGui")
+MainFrame = Instance.new("Frame")
+UICorner = Instance.new("UICorner")
+TextLabel = Instance.new("TextLabel")
+
+PopupMessage.Name = "AnomissPopup"
+PopupMessage.Parent = game:GetService("CoreGui")
+PopupMessage.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = PopupMessage
+MainFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+MainFrame.BackgroundTransparency = 0.500
+MainFrame.BorderColor3 = Color3.fromRGB(255, 255, 0)
+MainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+MainFrame.Size = UDim2.new(0, 600, 0, 30)
+
+UICorner.Parent = MainFrame
+
+TextLabel.Parent = MainFrame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.3611353703, 0, -0.4, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = text
+TextLabel.TextColor3 = Color3.fromRGB(colorr, colorg, colorb)
+TextLabel.TextSize = 40.000
+
+wait(time)
+PopupMessage:destroy()
+MainFrame:destroy()
+UICorner:destroy()
+TextLabel:destroy()
+end
+
 FAILEDLOADING = false
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Module.lua"))()
 local UI = Library.Load({
 	Title = "Fishing Simulator V3",
 	Style = 3,
-        SizeX = ScreenSizeX,
-	SizeY = ScreenSizeY,
-	Theme = Theme,
+        SizeX = 600,
+	SizeY = 400,
+	Theme = "Dark",
 	ColorOverrides = {
-		MainFrame = Color3.fromRGB(235,235,235 ),
-		Minimise = Color3.fromRGB(0, 0, 0 )
+	
 	}
 })
 --[--Set Values/Guis
@@ -94,6 +131,17 @@ game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
 end
 
+function switchServer()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
+
 if God then
 	local Cam = workspace.CurrentCamera
 	local Pos, Char = Cam.CFrame, speaker.Character
@@ -113,6 +161,158 @@ if God then
 		Script.Disabled = false
 	end
 	nHuman.Health = nHuman.MaxHealth
+end
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+ 
+ 
+-- Server Hop
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                end)
+                wait(4)
+            end
+        end
+    end
+end
+
+function ESP(plr)
+	task.spawn(function()
+		for i,v in pairs(COREGUI:GetChildren()) do
+			if v.Name == plr.Name..'_ESP' then
+				v:Destroy()
+			end
+		end
+		wait()
+		if plr.Character and plr.Name ~= Players.LocalPlayer.Name and not COREGUI:FindFirstChild(plr.Name..'_ESP') then
+			local ESPholder = Instance.new("Folder")
+			ESPholder.Name = plr.Name..'_ESP'
+			ESPholder.Parent = COREGUI
+			repeat wait(1) until plr.Character and getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+			for b,n in pairs (plr.Character:GetChildren()) do
+				if (n:IsA("BasePart")) then
+					local a = Instance.new("BoxHandleAdornment")
+					a.Name = plr.Name
+					a.Parent = ESPholder
+					a.Adornee = n
+					a.AlwaysOnTop = true
+					a.ZIndex = 10
+					a.Size = n.Size
+					a.Transparency = espTransparency
+					a.Color = plr.TeamColor
+				end
+			end
+			if plr.Character and plr.Character:FindFirstChild('Head') then
+				local BillboardGui = Instance.new("BillboardGui")
+				local TextLabel = Instance.new("TextLabel")
+				BillboardGui.Adornee = plr.Character.Head
+				BillboardGui.Name = plr.Name
+				BillboardGui.Parent = ESPholder
+				BillboardGui.Size = UDim2.new(0, 100, 0, 150)
+				BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
+				BillboardGui.AlwaysOnTop = true
+				TextLabel.Parent = BillboardGui
+				TextLabel.BackgroundTransparency = 1
+				TextLabel.Position = UDim2.new(0, 0, 0, -50)
+				TextLabel.Size = UDim2.new(0, 100, 0, 100)
+				TextLabel.Font = Enum.Font.SourceSansSemibold
+				TextLabel.TextSize = 20
+				TextLabel.TextColor3 = Color3.new(1, 1, 1)
+				TextLabel.TextStrokeTransparency = 0
+				TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+				TextLabel.Text = 'Name: '..plr.Name
+				TextLabel.ZIndex = 10
+				local espLoopFunc
+				local teamChange
+				local addedFunc
+				addedFunc = plr.CharacterAdded:Connect(function()
+					if ESPenabled then
+						espLoopFunc:Disconnect()
+						teamChange:Disconnect()
+						ESPholder:Destroy()
+						repeat wait(1) until getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+						ESP(plr)
+						addedFunc:Disconnect()
+					else
+						teamChange:Disconnect()
+						addedFunc:Disconnect()
+					end
+				end)
+				teamChange = plr:GetPropertyChangedSignal("TeamColor"):Connect(function()
+					if ESPenabled then
+						espLoopFunc:Disconnect()
+						addedFunc:Disconnect()
+						ESPholder:Destroy()
+						repeat wait(1) until getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+						ESP(plr)
+						teamChange:Disconnect()
+					else
+						teamChange:Disconnect()
+					end
+				end)
+				local function espLoop()
+					if COREGUI:FindFirstChild(plr.Name..'_ESP') then
+						if plr.Character and getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid") and Players.LocalPlayer.Character and getRoot(Players.LocalPlayer.Character) and Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
+							local pos = math.floor((getRoot(Players.LocalPlayer.Character).Position - getRoot(plr.Character).Position).magnitude)
+							TextLabel.Text = 'Name: '..plr.Name..' | Health: '..round(plr.Character:FindFirstChildOfClass('Humanoid').Health, 1)..' | Studs: '..pos
+						end
+					else
+						teamChange:Disconnect()
+						addedFunc:Disconnect()
+						espLoopFunc:Disconnect()
+					end
+				end
+				espLoopFunc = RunService.RenderStepped:Connect(espLoop)
+			end
+		end
+	end)
 end
 --]--End Of Functions
 
@@ -236,9 +436,14 @@ LocalI = Local.Toggle({
 --[--Other
 
 --Start Of Code
+OtherIInfo = People.Label({Text = "Dosen't Work Fixing Later!"})
 OtherI = People.Toggle({
 	Text = "ToggleESP",
 	Callback = function(Value)
+		ESPenabled = true
+		for i, v in pairs(game.Players:GetPlayers()) do
+				ESP(v)
+		end
 	end,
 	Enabled = false
 })
@@ -707,26 +912,33 @@ ConfigI = Config.Button({
 		end
 	}
 })
+ConfigI = Config.Button({
+	Text = "Rejoin",
+	Callback = function()
+		tpservice:Teleport(game.PlaceId, plr)
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "Rejoin your current game"
+			})
+		end
+	}
+})
+ConfigI = Config.Button({
+	Text = "ServerHop",
+	Callback = function()
+		switchServer()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "Joins a new server for you"
+			})
+		end
+	}
+})
 --]--End Of Config
-
-
-
---[--Premium
---Will Be Shown Once Premium Is Active
-
---Start Of Code
-PremiumI = PremiumUI.Label({Text = "Experimental Setting"})
-
-PremiumI = PremiumUI.Toggle({Text = "God", Callback = function(v)
-       if v then
-         God = true
-       elseif not v then
-	 God = false
-    end
-end, Enabled = false})
---]--End Of Premium
-
-
 
 
 
@@ -734,4 +946,25 @@ end, Enabled = false})
 wait(0.1)
 --Actual Fixes
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/log.lua", true))()
+CreateFloatingTextBox("Script Loaded Correctly!", 3, 25, 255, 25)
 --]--End Of Fix
+
+
+
+--[--Premium
+--Will Be Shown Once Premium Is Active
+
+--Start Of Code
+PremiumIInfo = PremiumUI.Label({Text = "Experimental Setting"})
+PremiumIInfo = PremiumUI.Toggle({
+	Text = "God-mode",
+	Callback = function(Value)
+		if Value then
+			God = true
+		else
+			God = false
+		end
+	end,
+	Enabled = false
+})
+--]--End Of Premium
