8a7928de2e2fb9ae329c3c3d5d3995ed472ddda5
DAa <103658363+Sittapea@users.noreply.github.com>
2023-03-09 22:23:29 +0200

    Update NewSetup.lua

--- NewSetup.lua
+++ NewSetup.lua
@@ -1,1 +1,246 @@
+--[[
 
+          ╭╭━━━━━╮╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮╭━━━━━╮╮
+          ┃┃ ━━━ ┃┃\       █████╗  ██████╗  █████╗ ███╗  ██╗██╗   ██╗      /┃┃ ━━━ ┃┃
+          ┃┃     ┃┃       ██╔══██╗██╔════╝ ██╔══██╗████╗ ██║╚██╗ ██╔╝       ┃┃     ┃┃
+          ┃┃     ┃┃       ███████║██║  ██╗ ██║  ██║██╔██╗██║ ╚████╔╝        ┃┃     ┃┃
+          ┃┃     ┃┃       ██╔══██║██║  ╚██╗██║  ██║██║╚████║  ╚██╔╝         ┃┃     ┃┃
+          ┃┃     ┃┃       ██║  ██║╚██████╔╝╚█████╔╝██║ ╚███║   ██║          ┃┃     ┃┃
+          ┃┃ ━━━ ┃┃/      ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚═╝  ╚══╝   ╚═╝         \┃┃ ━━━ ┃┃
+          ╰╰━━━━━╯╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯╰━━━━━╯╯
+ 
+╭━━━━━━━━━━━━━━━━╮╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮╭━━━━━━━━━━━━━━━━╮
+                      █▀▀▀█ █▀▀█ █──█ █▀▀█ █▀▀ █▀▀ 　 █▀▀█ █▀▀█ █▀▀▄ █▀▀
+                     ─▀▀▀▄▄ █──█ █──█ █▄▄▀ █── █▀▀ 　 █─── █──█ █──█ █▀▀
+                      █▄▄▄█ ▀▀▀▀ ─▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀ 　 █▄▄█ ▀▀▀▀ ▀▀▀─ ▀▀▀
+╰━━━━━━━━━━━━━━━━╯╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯╰━━━━━━━━━━━━━━━━╯
+
+--]]
+
+local MainGui = Instance.new("ScreenGui")
+MainGui.Name = "ApocGui"
+MainGui.Parent = game.CoreGui
+
+local ZeroPhrame = Instance.new("Frame")
+ZeroPhrame.Size = UDim2.new(0, 195, 0, 115)
+ZeroPhrame.Position = UDim2.new(0, 200, 0, 200)
+ZeroPhrame.BorderSizePixel = 5
+ZeroPhrame.Transparency = 1
+ZeroPhrame.Draggable = true
+ZeroPhrame.Active = true
+ZeroPhrame.Selectable = true
+ZeroPhrame.Parent = MainGui
+
+local LoaderPhrame = Instance.new("Frame")
+LoaderPhrame.Size = UDim2.new(0, 195, 0, 30)
+LoaderPhrame.Position = UDim2.new(0, 0, 0, 0)
+LoaderPhrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+LoaderPhrame.Style = 3
+LoaderPhrame.BorderSizePixel = 5
+LoaderPhrame.Transparency = 0.1
+LoaderPhrame.Active = true
+LoaderPhrame.Selectable = true
+LoaderPhrame.Parent = ZeroPhrame
+
+local UpdatesPhrame = Instance.new("Frame")
+UpdatesPhrame.Size = UDim2.new(0, 195, 0, 90)
+UpdatesPhrame.Position = UDim2.new(0, 0, 0, 30)
+UpdatesPhrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+UpdatesPhrame.Style = 6
+UpdatesPhrame.BorderSizePixel = 5
+UpdatesPhrame.Transparency = 0.1
+UpdatesPhrame.Active = true
+UpdatesPhrame.Selectable = true
+UpdatesPhrame.Parent = ZeroPhrame
+
+local UpdatesPhrameSeperator = Instance.new("Frame")
+UpdatesPhrameSeperator.Size = UDim2.new(0, 180, 0, 2)
+UpdatesPhrameSeperator.Position = UDim2.new(0, 0, 0.25, 0)
+UpdatesPhrameSeperator.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+UpdatesPhrameSeperator.Style = 3
+UpdatesPhrameSeperator.BorderSizePixel = 5
+UpdatesPhrameSeperator.Transparency = 0.1
+UpdatesPhrameSeperator.Active = true
+UpdatesPhrameSeperator.Selectable = true
+UpdatesPhrameSeperator.Parent = UpdatesPhrame
+
+local UpdateLabel = Instance.new("TextLabel")
+UpdateLabel.Size = UDim2.new(0, 0, 0, 0)
+UpdateLabel.Position = UDim2.new(0.5, 0, 0.10, 0)
+UpdateLabel.BorderSizePixel = 0
+UpdateLabel.Text = "Version: "..versionCURRENT
+UpdateLabel.Transparency = 0.9
+UpdateLabel.TextColor3 = Color3.fromRGB(0,255,255)
+UpdateLabel.TextSize = 12
+UpdateLabel.TextXAlignment = "Center"
+UpdateLabel.Parent = UpdatesPhrame
+
+local UpdateLabel = Instance.new("TextLabel")
+UpdateLabel.Size = UDim2.new(0, 0, 0, 0)
+UpdateLabel.Position = UDim2.new(0.5, 0, 0.40, 0)
+UpdateLabel.BorderSizePixel = 0
+UpdateLabel.Text = "loading might take some time!"
+UpdateLabel.TextColor3 = Color3.fromRGB(140,140,140)
+UpdateLabel.TextSize = 9
+UpdateLabel.TextXAlignment = "Center"
+UpdateLabel.Parent = UpdatesPhrame
+
+local UpdateLabel = Instance.new("TextLabel")
+UpdateLabel.Size = UDim2.new(0, 0, 0, 0)
+UpdateLabel.Position = UDim2.new(0.5, 0, 0.65, 0)
+UpdateLabel.BorderSizePixel = 0
+UpdateLabel.Text = "is new loader better?"
+UpdateLabel.TextColor3 = Color3.fromRGB(140,140,140)
+UpdateLabel.TextSize = 9
+UpdateLabel.TextXAlignment = "Center"
+UpdateLabel.Parent = UpdatesPhrame
+
+local UpdateLabelScript = Instance.new("TextLabel")
+UpdateLabelScript.Size = UDim2.new(0, 0, 0, 0)
+UpdateLabelScript.Position = UDim2.new(0.5, 0, 0.9, 0)
+UpdateLabelScript.BorderSizePixel = 0
+UpdateLabelScript.Text = "idk welcome ig"
+UpdateLabelScript.TextColor3 = Color3.fromRGB(140,140,140)
+UpdateLabelScript.TextSize = 9
+UpdateLabelScript.TextXAlignment = "Center"
+UpdateLabelScript.Parent = UpdatesPhrame
+
+local LoadingBar = Instance.new("TextButton")
+LoadingBar.Size = UDim2.new(0, 100, 0, 15)
+LoadingBar.Position = UDim2.new(0, 0, 0, 0)
+LoadingBar.BackgroundColor3 = Color3.fromRGB(0, 255, 255)
+LoadingBar.BorderColor3 = Color3.fromRGB(255, 255, 255)
+LoadingBar.Text = ""
+LoadingBar.BorderSizePixel = 0
+LoadingBar.Transparency = 0
+LoadingBar.BackgroundTransparency = 0.9
+LoadingBar.TextColor3 = Color3.fromRGB(255,255, 255)
+LoadingBar.TextSize = 8
+LoadingBar.TextXAlignment = "Center"
+LoadingBar.Parent = LoaderPhrame
+
+local LoadingFill = Instance.new("TextButton")
+LoadingFill.Size = UDim2.new(0, 180, 0, 15)
+LoadingFill.Position = UDim2.new(0, 0, 0, 0)
+LoadingFill.BorderColor3 = Color3.fromRGB(255, 255, 255)
+LoadingFill.Text = "Loading 0/100%"
+LoadingFill.BorderSizePixel = 1
+LoadingFill.Transparency = 0
+LoadingFill.BackgroundTransparency = 0.9
+LoadingFill.TextColor3 = Color3.fromRGB(255,255, 255)
+LoadingFill.TextSize = 8
+LoadingFill.TextXAlignment = "Center"
+LoadingFill.Parent = LoaderPhrame
+
+local function StartLoading(Val, ValQ, Time, Amount)
+for i = 0, Amount do
+local LoadedBarAmount = math.floor(0+(i/Amount*ValQ))
+local LoadedAmount = math.floor(0+(i/Amount*Val))
+LoadingFill.Text= "Loading "..tostring(LoadedAmount).."/100%"
+LoadingBar.Size = UDim2.new(0, tostring(LoadedBarAmount), 0, 15)
+wait(Time)
+end
+end
+
+local MakeUpLoadingBar = math.random(20, 100)
+StartLoading(100, 180, 0.1, tostring(MakeUpLoadingBar))
+
+--[--Run Checks
+
+--Anti Bypass Modules
+FishingSimulatorCURRENT = 2
+DoorsCURRENT = 2
+ApocCURRENT = 1
+ThemeParkTycoon2CURRENT = 1
+UltimateDrivingCURRENT = 1
+
+FAILEDLOADING = nil
+ISUNIVERSAL = false
+OUTDATEDVERSION = nil
+DOORSISPATCHED = nil
+APOCISPATCHED = nil
+FSISPATCHED = nil
+TPT2ISPATCHED = nil
+UDISPATCHED = nil
+--End Of ABM
+
+--]--Finished
+
+
+
+--[--Setup
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Version.lua", true))()
+FixedThemes = Theme == "Aqua" or Theme == "Light" or Theme == "Dark" or Theme == "Jester" or Theme == "Mocha"
+BrokenThemes = Theme ~= "Aqua" or Theme ~= "Light" or Theme ~= "Dark" or Theme ~= "Jester" or Theme ~= "Mocha" or Theme == nil
+IsPatched = TPT2ISPATCHED == false and UDISPATCHED == false and FSISPATCHED == false and DOORSISPATCHED == false and ApocISPATCHED == false
+ScreenCheckX = ScreenSizeX ~= nil or ScreenSizeX ~= nan
+ScreenCheckY = ScreenSizeY ~= nil or ScreenSizeY ~= nan
+IsAGameThatWorks = game.PlaceId == 2866967438 or game.PlaceId == 237590761 or game.PlaceId == 69184822 or game.PlaceId == 54865335 or game.PlaceId == 6516141723 or game.PlaceId == 237590657 or game.PlaceId == 1228674372 or game.PlaceId == 1600503 or game.PlaceId == 237590761
+wait(0.1)
+--]--End Of Setup
+
+
+
+--[--Script
+if game.PlaceId == 2866967438 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and FSISPATCHED == false and ScreenCheckX and ScreenCheckY then
+UpdateLabelScript.Text = "Fishing Simulator Script Is Loading"
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Fishing%20Simulator.lua", true))()
+  MainGui:destroy()
+	
+elseif game.PlaceId == 69184822 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and TPT2ISPATCHED == false and ScreenCheckX and ScreenCheckY then
+UpdateLabelScript.Text = "Theme Park Tycoon 2 Script Is Loading"
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/ThemeParkTycoon2.lua", true))()
+  MainGui:destroy()
+	
+elseif game.PlaceId == 54865335 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and UDISPATCHED == false and ScreenCheckX and ScreenCheckY then
+UpdateLabelScript.Text = "Ultimate Driving Script Is Loading"
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Ultimate%20Driving.lua", true))()
+  MainGui:destroy()
+	
+elseif game.PlaceId == 6516141723 or game.PlaceId == 6839171747 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and DOORSISPATCHED == false and ScreenCheckX and ScreenCheckY then
+UpdateLabelScript.Text = "Doors Script Is Loading"
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Doors.lua", true))()
+  MainGui:destroy()
+	
+elseif game.PlaceId == 237590761 or game.PlaceId == 237590657 or game.PlaceId == 1228674372 or game.PlaceId == 1600503 or game.PlaceId == 237590761 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and ApocISPATCHED == false and ScreenCheckX and ScreenCheckY then
+UpdateLabelScript.Text = "MultiScript Script Is Loading"
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/apocloader.lua", true))()
+  MainGui:destroy()
+	
+elseif IsAGameThatWorks and BrokenThemes and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and IsPatched and ScreenCheckX and ScreenCheckY then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 1: wrong theme / no theme"
+
+elseif IsAGameThatWorks and Discord_Key ~= "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 2: wrong discord key"
+	
+elseif IsAGameThatWorks and OUTDATEDVERSION == true and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 3: invalid version"
+	
+elseif game.PlaceId ~= 237590657 or game.PlaceId ~= 1228674372 or game.PlaceId ~= 237590761 or game.PlaceId ~= 1600503 or game.PlaceId ~= 237590761 or game.PlaceId ~= 2866967438 or game.PlaceId ~= 69184822 or game.PlaceId ~= 54865335 and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and IsPatched and ScreenCheckX and ScreenCheckY then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 4: unsupported game"
+
+wait(10)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/UniversalScript.lua", true))()
+  MainGui:destroy()
+
+elseif IsAGameThatWorks and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and DOORSISPATCHED == true or TPT2ISPATCHED == true or UDISPATCHED == true or APOCISPATCHED == true or FSISPATCHED == true and ScreenCheckX and ScreenCheckY then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 5: script is patched"
+	
+elseif IsAGameThatWorks and ScreenSizeX == nil or ScreenSizeX == nan or ScreenSizeY == nil or ScreenSizeY == nan and OUTDATEDVERSION == false and Discord_Key == "Ey580OgEJIYnagHJUIWAjfasnfg" and FixedThemes and IsAGameThatWorks then
+wait(0.1)
+  UpdateLabelScript.Text = "Error 6: invalid screensize"
+	
+end
+
+--]--End Of Script
