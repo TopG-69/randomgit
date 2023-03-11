11d5cba54856aca39296f8ffdd9f40d680f385e7
DAa <103658363+Sittapea@users.noreply.github.com>
2023-03-11 17:17:39 +0200

    Update Fishing Simulator.lua

--- Fishing Simulator.lua
+++ Fishing Simulator.lua
@@ -1,5 +1,5 @@
 FAILEDLOADING = false
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/-/main/Module.lua?token=GHSAT0AAAAAAB7C53BGY6PILSARBIYA4Y5EZAMSXKA"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/-/main/Module.lua?token=GHSAT0AAAAAAB7C53BGLYQDNR2TLNDM4MQ4ZAMTEKA"))()
 local UI = Library.Load({
 	Title = "Fishing Simulator V3",
 	Style = 3,
