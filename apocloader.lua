cfe09da1d4f5f4590a28109af303c4c89a931d39
DAa <103658363+Sittapea@users.noreply.github.com>
2023-06-05 15:18:29 +0300

    Update apocloader.lua

--- apocloader.lua
+++ apocloader.lua
@@ -1,3 +1,10 @@
+--[[
+	Hey if you are seeing this then it means you are at the source
+	of agony well i'm here to tell you Agony will make a returns
+	at somepoint but i wish for you to make a promise to never
+	share this with anyone. Goodbye!
+--]]
+
 local Popup = Instance.new("ScreenGui")
 local MainFrame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
