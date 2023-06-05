8edcf38e9425194ed75ff021e42edbc57727cb0c
DAa <103658363+Sittapea@users.noreply.github.com>
2023-06-05 15:17:01 +0300

    Update NewSetup.lua

--- NewSetup.lua
+++ NewSetup.lua
@@ -15,6 +15,13 @@
 ╰━━━━━━━━━━━━━━━━╯╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯╰━━━━━━━━━━━━━━━━╯
 --]]
 
+--[[
+	Hey if you are seeing this then it means you are at the source
+	of agony well i'm here to tell you Agony will make a returns
+	at somepoint but i wish for you to make a promise to never
+	share this with anyone. Goodbye!
+--]]
+
 --[[ Req Features --]]
 local MainGui = Instance.new("ScreenGui")
 MainGui.Name = "ApocGui"
