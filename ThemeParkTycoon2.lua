ec0dc4cfe83b43bd1e3c3bdf0aa9283e401c771f
DAa <103658363+Sittapea@users.noreply.github.com>
2023-06-05 15:17:52 +0300

    Update ThemeParkTycoon2.lua

--- ThemeParkTycoon2.lua
+++ ThemeParkTycoon2.lua
@@ -1,3 +1,10 @@
+--[[
+	Hey if you are seeing this then it means you are at the source
+	of agony well i'm here to tell you Agony will make a returns
+	at somepoint but i wish for you to make a promise to never
+	share this with anyone. Goodbye!
+--]]
+
 FAILEDLOADING = false
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/randomgit/main/Module.lua"))()
 local UI = Library.Load({
